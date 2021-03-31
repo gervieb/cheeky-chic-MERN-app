fc1c9c10dbbc6d674203267ddbc64b86a9598f6b
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-03-31 18:03:09 +0200

    added order confirmation

---
+++ client/src/containers/OrderConfirmation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function OrderConfirmation() {
+    return (
+        <div>
+            <p>Thank you for your order name</p>
+            <p>You order confirmation has been email to emailaddress</p>
+        </div>
+    )
+}
