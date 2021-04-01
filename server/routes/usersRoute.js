13379e673dcd19f11c7cca09a3650abfc06d6bf4
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-01 20:57:47 +0200

    added privacy policy

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -6,5 +6,6 @@ router.get('/', controller.find);
 router.post('/register', controller.register);
 router.post('/login', controller.login);
 router.post('/verify_token', controller.verify_token);
+// router.post('/update/:id', controller.update_user_info);
 
 module.exports = router;
