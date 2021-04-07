fc99f6e2310efb5ee0d3f17f5a16dfb7cfb9d66f
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-07 17:33:21 +0200

    fixed nodemailer confirmation email

--- server/controllers/emailsController.js
+++ server/controllers/emailsController.js
@@ -34,7 +34,7 @@ class EmailsController {
     }
 
     confirmation_email = async (req, res) => {
-        let { data } = req.body
+        let { data, total, cart } = req.body
         
         const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -42,19 +42,23 @@ class EmailsController {
             user: em,
             pass: pwd
         },
+        tls: {
+            rejectUnauthorized: false
+        }
     });
 
     const mailOptions = {
         from: em,
         to: data.email,
         subject: 'Order Confirmation',
-        html: ` <h4>Hello ${details.name}, thank you for your order</h4>
+        html: ` <h4>Hello ${data.firstName} ${data.lastName}, thank you for your order.</h4>
                 <p>We've received your order and will contact you as soon as your package is shipped.
                 You can find your purchase information below.</p>
-                <h3>Order Summary</h3>
-                <p>${details.shoppingCart.map(ele=>ele.title
-                )}</p>
-                <p>total price: ${details.total}€</p> `
+                <br /> <br />
+                <h3> Your Order Summary</h3>
+                ${cart.map(ele => ele.title 
+                )}
+                <p>total price: ${total}€</p>`
     };
     transporter.sendMail(mailOptions, (error, info) => {
         let response;
