c07b4a932071607f8c62764216af1bcbb6e9ed81
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-07 19:05:52 +0200

    removed unnecessary data

--- client/src/containers/ItemDetails.js
+++ client/src/containers/ItemDetails.js
@@ -4,9 +4,12 @@ import FlashMessage from 'react-flash-message'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { customInstance as axios } from '../config.js'
 
-export default function ItemDetails({ match, onAdd, isAddedToCart, itemAddedToCart, setIsAddedToCart}) {
+export default function ItemDetails(props) {
+  const { match, onAdd, isAddedToCart, itemAddedToCart, setIsAddedToCart} = props
 
   const [item, setItem] = useState({});
+  
+  console.log('this is item', item)
   
   let history = useHistory()
   
@@ -32,33 +35,49 @@ export default function ItemDetails({ match, onAdd, isAddedToCart, itemAddedToCa
     }
   };
 
+  // const increaseQuantity = (itemQty) => {
+  //   setItem({...item, qty: itemQty + 1} )
+  // }
+
+  // const decreaseQuantity = (itemQty) => {
+  //   itemQty > 1? setItem({...item, qty: itemQty - 1} ): 
+  //   setItem({...item, qty: 1})
+  // }
+
   return ( 
-    <div className="product-wrapper grid-2-item">
-      <div>
-        <img src={item.image} alt={item.title} />
+    <div className="product-wrapper">
+      <div className="grid-2-item">
+        <div>
+          <img src={item.image} alt={item.title} />
+        </div>
+        <div className="product-details">
+          <ul>
+            <li><h3 className="item-title">{item.title}</h3></li>
+            <li><p>{item.description}</p></li>
+            <li><label><strong>Price:</strong> €{item.price} </label></li>
+            <br />
+            {/* <li className="quantity-wrapper">
+                <button onClick={() => decreaseQuantity(item.qty)}>-</button>                    
+                  {item.qty}                    
+                <button onClick={() => increaseQuantity(item.qty)}>+</button>
+            </li> */}
+            <li>
+              <button type="submit" onClick={() => onAdd(item)}>add to cart</button>
+              {isAddedToCart?
+              <div>
+                <FlashMessage duration={3000}>
+                  <p style={{color: 'red', fontSize: 'small'}}>{itemAddedToCart.title} has been added to your cart!</p>
+                </FlashMessage>
+                </div>: null
+                }   
+            </li>
+          </ul>
+        </div> 
       </div>
-      <div className="product-details">
-        <ul>
-          <li><h3 className="item-title">{item.title}</h3></li>
-          <li><p>{item.description}</p></li>
-          <li><label><strong>Price:</strong> €{item.price} </label></li>
-          <br />
-          <li>
-            <button type="submit" onClick={() => onAdd(item)}>add to cart</button>
-            {isAddedToCart?
-            <div>
-              <FlashMessage duration={3000}>
-                <p style={{color: 'red', fontSize: 'small'}}>{itemAddedToCart.title} has been added to your cart!</p>
-              </FlashMessage>
-              </div>: null
-              }   
-          </li>
-        </ul>
-      </div> 
-      <div className="go-back-flex">
+        <div className="go-back-flex">
           <ChevronLeftIcon />
-          <p className="go-back" onClick={goBack}>Back</p>
-      </div>
+          <p className="go-back" onClick={goBack}>Back To Shop</p>
+        </div>      
     </div>
   );
 }
