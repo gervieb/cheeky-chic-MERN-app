a84a1ac3013236eb56475c9d0c6f54e9bc1d2d42
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-07 14:46:53 +0200

    added user data

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -5,6 +5,13 @@ const usersSchema = new mongoose.Schema({
     firstName:{type:String, unique: false, required: true},
     lastName:{type:String, unique: false, required: true},
     email:{type:String, unique: true, required: true, lowercase: true},
+    phone: {type: Number, unique: false, required: true},
+    street:{type:String, unique: false, required: true},
+    address2:{type:String, unique: false, required: false},
+    city:{type:String, unique: false, required: true},
+    postcode:{type:String, unique: false, required: true},
+    state:{type:String, unique: false, required: true},
+    country:{type:String, unique: false, required: true},
     password:{type:String, unique: false, required: true, minlength: 6 },
     password2:{type:String, unique: false, required: true, minlength: 6 },
     admin: {type:Boolean, default:false}
