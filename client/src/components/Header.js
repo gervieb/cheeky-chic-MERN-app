0d334e9c58f496a6237e82e678db6c6211d8c4dd
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-11 06:55:54 +0200

    added search bar functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BsSearch } from 'react-icons/bs'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { customInstance as axios } from '../config.js'
+import CloseIcon from '@material-ui/icons/Close';
 
 
-export default function Header({cartLength, isLoggedIn }) {
+export default function Header({ cartLength, isLoggedIn }) {
+    const [search, setSearch] = useState("");
+    const [allProducts, setAllProducts] = useState([])
+    const [searchbar, setSearchbar] = useState(false)
+    let history = useHistory();
 
-    
+    useEffect(() => {
+        getAllProducts()
+    }, [])
+
+
+    const showSearchbar =()=> {
+        setSearchbar(searchbar? false: true)
+    }
+
+
+    const getAllProducts = async() => {
+        try {
+            const response = await axios.get('/products/');
+            setAllProducts(response.data.products)
+          } catch (error) {
+            console.log("error ==>", error);
+          }
+    }     
+
+    const handleSearch =(e)=> {
+        setSearch(e.target.value.toLowerCase())
+    }
+
+    const searchProduct = async(e) => {
+        e.preventDefault()
+        filtered(allProducts);  
+    }
+
+    const filtered = (allProducts) => {
+        let filteredProd = allProducts.filter((product) => {  
+            return (
+            product.title.toLowerCase().includes(search) ||
+            product.category.toLowerCase().includes(search) ||
+            product.description.toLowerCase().includes(search)
+            )
+        }); 
+
+        history.push({
+            pathname: "/search",
+            state: filteredProd,
+            products: allProducts}) 
+    }
+
+    const clearSearch =()=> {
+        setSearch("");
+    }
+
 
     return (
         <div className="header-container"> 
@@ -15,8 +67,19 @@ export default function Header({cartLength, isLoggedIn }) {
                 <h1 className="shop-title">Cheeky-Chic</h1>
             </Link> 
             <div className="search-bar-wrapper">
-                <BsSearch className="search-bar-icon"/>
-                <input className="search-bar" placeholder="Search"></input>              
+                <form onSubmit={searchProduct}>
+                    {!searchbar?
+                    <BsSearch onClick={showSearchbar} className="search-bar-icon"/>:
+                    <CloseIcon onClick={showSearchbar}/>
+                    }
+                    <div className={searchbar? "show-searchbar" : "searchbar-hidden"}>
+                        <input className="search-bar" placeholder="What are you searching for?" value={search} onChange={handleSearch}/>
+                        {search.length > 0 &&
+                        <span onClick={clearSearch}>Clear</span>
+                        }
+                        <span onClick={searchProduct}><BsSearch /> </span>
+                    </div>  
+                </form>      
                 <Link to={"/cart"}>
                     <div className="cart-icon">
                         <ShoppingCartIcon />  
