d2f4992c8708542d5b804496fed77fca0d3cc59b
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-21 01:51:46 +0200

    removed menu icon

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import AdminHeader from '../components/AdminHeader'
 import { SidebarData } from './SidebarData'
-import MenuIcon from '@material-ui/icons/Menu';
 import CancelIcon from '@material-ui/icons/Cancel';
 
 export default function Sidebar({isAdmin}) {
@@ -13,7 +12,7 @@ export default function Sidebar({isAdmin}) {
     return (<div className='sidebar-wrapper'>
         {isAdmin?
         <>
-            <AdminHeader /> 
+            <AdminHeader showSidebar={showSidebar}/> 
             <div className="admin-sidebar">  
                 <nav className='sidebar-menu active'>                                     
                     <ul className='sidebar-menu-items'  >   
@@ -31,11 +30,6 @@ export default function Sidebar({isAdmin}) {
                     </ul>
                 </nav>
             </div> 
-                <div className="sidebar">
-                    <Link to='#' className='menu-bars'>
-                        <MenuIcon onClick={showSidebar}/>
-                    </Link>
-                </div>
                 <nav className={sidebar? 'sidebar-menu active' : 'sidebar-menu'}>
                     <ul className='sidebar-menu-items' onClick={showSidebar} > 
                         <li className='sidebar-toggle'>
