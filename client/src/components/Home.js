b7b1e2e67327b63b864b1e2f382420a8e2481fc3
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-21 01:54:55 +0200

    added item collections

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,21 +3,24 @@ import { Carousel } from "react-responsive-carousel";
 import { Link } from 'react-router-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import style from "../customStyles.css";
+import bag from "../images/bag.jpg"
+import apparel from "../images/apparel.jpg"
+import shoes from "../images/shoes.jpg"
+import watch from "../images/watches.jpg"
+import jewelry from "../images/jewelry.jpg"
 
-export default function Home({ productData }) {
+export default function Home(props) {
   const [data, setData] = useState([]);
-  
 
   useEffect(() => {
-    feauturedItems(productData)
-  }, [productData])
-  
-  
+    featuredItems(props.productData)
+  }, [props.productData])
 
-  const feauturedItems = (prod)=> {
-    const sliced = prod.slice(0, 4);
+
+  const featuredItems = (product)=> {
+    const sliced = product.slice(0, 4);
     setData(sliced)
-  } 
+  }
 
   return (
     <div className="featured-products">    
@@ -33,6 +36,30 @@ export default function Home({ productData }) {
             </div>
           ))}
         </Carousel>
+        
+        <h2 className="fashion-collection">OUR FASHION COLLECTION</h2>
+        <div className="collection-wrapper">         
+          <div className="img-box">
+          <img src={apparel} alt="apparel" />
+            <Link to={{pathname: "/product-by-category", data: "apparel"}}><h2 className="collection-label">Apparel</h2></Link>
+          </div>
+          <div className="img-box">
+            <img src={shoes} alt="shoes" />
+            <Link to={{pathname: "/product-by-category", data: "shoes"}}><h2 className="collection-label">Shoes</h2></Link>
+          </div>
+          <div className="img-box">
+            <img src={watch} alt="watch" />
+            <Link to={{pathname: "/product-by-category", data: "watches"}}><h2 className="collection-label">Watches</h2></Link>
+          </div>
+          <div className="img-box">
+            <img src={bag} alt="bags" />
+            <Link to={{pathname: "/product-by-category", data: "bags"}}><h2 className="collection-label">Bags</h2></Link>
+          </div>
+          <div className="img-box">
+            <img src={jewelry} alt="jewelry" />
+            <Link to={{pathname: "/product-by-category", data: "jewelry"}}><h2 className="collection-label">Jewelry</h2></Link>
+          </div>
+        </div> 
     </div>
-  );
+  )
 }
