754098dec4998c832cd9a570664fda41e567cd00
Gervie Barczyk <gerviebarczyk@gmail.com>
2021-04-25 23:23:13 +0200

    added cart toast

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import toast, { Toaster } from 'react-hot-toast';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const notify = () => toast('1 product added to cart');
+toast.configure()
 
 export default function Products({ productData, onAdd, showAllProducts }) {
 
@@ -10,13 +11,16 @@ export default function Products({ productData, onAdd, showAllProducts }) {
        showAllProducts() // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []) 
 
+    const notify = ()=>{ 
+        toast('1 product added to cart') 
+    }
+
     return ( 
         <div className="grid-2">  
             {productData.map((product, id)=> 
                 <div key={id} className="img-wrap">                   
                         <img src={product.image} alt={product.title} /> 
-                        <p className="img-add-cart" onClick={() => {onAdd(product); notify()}}>Add To Cart</p> 
-                        <Toaster/>                
+                        <p className="img-add-cart" onClick={() => {onAdd(product); notify()}}>Add To Cart</p>                 
                     <Link to={`./products/${product._id}`}>                
                         <h4 className="product-title">{product.title}</h4>
                     </Link>
